6cb51adf695d2e43b130884fc93a76b0e3071c69
AbuSaid <brodyaga8556@gmail.com>
2025-08-18 16:04:56 +0500

    BRR

---
+++ main/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class CoreViewsTest(TestCase):
+    def test_index_page(self):
+        # Проверяем, что страница / работает
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Hello, CI/CD!")
+        self.assertTemplateUsed(response, "main/index.html")
