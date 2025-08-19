d98dc953312dfa0cc52ffc964a465528c501da97
AbuSaid <brodyaga8556@gmail.com>
2025-08-19 10:53:59 +0500

    test 1

--- main/views.py
+++ main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, CI/CD!")
+    return HttpResponse("Hello, CI/CD!\nThis is a simple Django application demonstrating CI/CD deployment.")
